3ff14736bc44c680825379e27e35cb166b32ec78
KainaniD <kainanime2@gmail.com>
2024-05-31 16:01:10 -0700

    added password hashing and finalized room creation/deletion

--- server/server.js
+++ server/server.js
@@ -2,7 +2,14 @@ const express = require('express');
 const app = express();
 const PORT = 4000;
 const mongoose = require('mongoose')
+const passport = require('passport')
+const session = require('express-session')
+
 const userModel = require('./model/User')
+passport.use(userModel.createStrategy());   
+passport.serializeUser(userModel.serializeUser());
+passport.deserializeUser(userModel.deserializeUser());
+
 const roomModel = require('./model/Rooms')
 
 const http = require('http');
@@ -12,51 +19,76 @@ const cors = require('cors');
 mongoose.connect('mongodb+srv://keelanhu01:yjX6GhQTrZhNDkJs@tabletop-simulator.g6o2qfy.mongodb.net/?retryWrites=true&w=majority&appName=Tabletop-Simulator')
 app.use(express.json())
 app.use(cors());
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false,
+    cookie: { secure: true }
+}))
 
+app.use(passport.session());
+  
 app.post("/users", (req, res) => {
-
-    const {name} = req.body;
+    const {username, email, password} = req.body;
     
-    userModel.findOne({name : name})
+    userModel.findOne({username : username})
     .then((user) => {
         if(user) {
-            res.json("Failed")
+            res.json({success: false, message: "a user with the name " + username + " already exists"})
         }else{
-            userModel.create(req.body)
-            .then(users => res.json(users))
-            .catch(err => res.json(err))
+            userModel.register(new userModel ({username: username, email: email}), password)
+            .then (res.json( {success: true, message: "Hi" + username + " you've registered your account!"}))
+            .catch (err => res.json(err))
         }
     })
 })
 
 app.get("/users", (req, res) => {
-    const {name, password} = req.query
-    userModel.findOne({name : name})
-    .then((user) => {
-        if(user) {
-            if(user.password === password){
-                res.json("Success")
-            }else{
-                res.json("Failed")
+    const {username, password} = req.query
+    req.body = {username: username, password: password}
+    passport.authenticate("local", function (err, user) { 
+        if (err) { 
+            res.json({ success: false, message: "something wrong happened :(" }); 
+        } 
+        else { 
+            if (!user) { 
+                res.json({ success: false, message: "username or password incorrect" }); 
+            } else {
+                req.login(user, function(err) {
+                    if (err) {
+                        res.json({ success: false, message: "something wrong happened :(" }); 
+                    } else {
+                        res.json({ success: true, message: "you logged in", user: user})
+                    }
+                })
             }
-        }else{
-            res.json("No record existed")
-        }
-    })
+        } 
+    })(req, res); 
 })
 
 app.post("/rooms", (req, res) => {
-    roomModel.create(req.body)
-    .then(rooms => res.json(rooms))
-    .catch(err => res.json(err))
+
+    const {name} = req.body;
+    
+    roomModel.findOne({name : name})
+    .then((room) => {
+        if(room) {
+            res.json({success: false, message: "there already exists a room with this name"})
+        }else{
+            roomModel.create(req.body)
+            .then(res.json({success: true, message: "room created"}))
+            .catch(err => res.json(err))
+        }
+    })
+
 })
 
 app.get("/rooms", (req, res) => {
     const {id, condition} = req.query
     if (condition === "delete") {
         roomModel.deleteOne({name : id})
-        .then(res.json("successfully deleted"))
-        .catch(err => res.json(err))
+        .then(res.json({ success: true, message: "successfully deleted"}))
+        .catch(res.json({ success: false, message: "failed to delete" }))
     } else if (condition === "query") {
         roomModel.find({})
         .then((room) => {
