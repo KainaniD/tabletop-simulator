266d852f5c235525723b58f55230ad19a8422a4d
Patrick Zhou <patrickxc.zhou@gmail.com>
2024-06-01 02:00:07 -0700

    forgot to comment something out

--- client/src/App.js
+++ client/src/App.js
@@ -30,10 +30,10 @@ function App() {
       <Link to="/profile" className="nav-button">Profile</Link>
       </div>
     } else {
-      return <div className="flex justify-evenly bg-gray-100 py-5 mb-5 border border-gray-300">
-      <Link to="/register" className="nav-button">Register</Link>
-      <Link to="/login" className="nav-button">Login</Link>
-      </div>
+      // return <div className="flex justify-evenly bg-gray-100 py-5 mb-5 border border-gray-300">
+      // <Link to="/register" className="nav-button">Register</Link>
+      // <Link to="/login" className="nav-button">Login</Link>
+      // </div>
     }
   }
 
