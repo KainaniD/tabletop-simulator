3d9056806e0600332a40f2056520a26656d6ed36
KainaniD <kainanime2@gmail.com>
2024-06-01 15:45:21 -0700

    users cannot access pages when they are not logged in by changing the url

--- client/src/App.js
+++ client/src/App.js
@@ -18,6 +18,7 @@ function App() {
         }
     })
     .catch(err => console.log(err))
+
   }, [])
 
 
--- client/src/MyRoutes.js
+++ client/src/MyRoutes.js
@@ -1,3 +1,4 @@
+import React, { useEffect, useState } from 'react'
 import { Route, Routes } from 'react-router-dom'
 import { Home } from './pages/Home'
 import { Room } from './pages/Room'
@@ -7,22 +8,59 @@ import { Profile } from './pages/Profile'
 import { NewRoom } from './pages/NewRoom'
 import { Register } from './pages/Register'
 import { Login } from './pages/Login'
+import axios from './axiosConfig'
 
 export const MyRoutes = () => {
+
+    const [isLoggedIn, setIsLoggedIn] = useState(false)
+
+
+    useEffect(() => {
+      axios.get("http://localhost:4000/session")
+      .then((result) => {
+          if (result.data) {
+            setIsLoggedIn(true)
+          } else {
+            setIsLoggedIn(false)
+          }
+      })
+      .catch(err => console.log(err))
+  
+    }, [])
+
+
+    function routes(){
+        if (isLoggedIn === true){
+          return (
+          <>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/rooms">
+                    <Route index element={<GameRooms />} />
+                    <Route path=":id" element={<Room />} />
+                    <Route path="newroom" element={<NewRoom />} />   
+                </Route>
+                <Route path="/profile" element={<Profile />} />
+                <Route path="/register" element={<Register />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="*" element={<NotFound />} />
+            </Routes>
+          </>
+          )
+        } else {
+          return (
+          <>
+            <Routes>
+                <Route path="/register" element={<Register />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="*" element={<NotFound />} />
+            </Routes>
+          </>
+          )
+        }
+    }
+
     return (
-        <>
-        <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/rooms">
-                <Route index element={<GameRooms />} />
-                <Route path=":id" element={<Room />} />
-                <Route path="newroom" element={<NewRoom />} />   
-            </Route>
-            <Route path="/profile" element={<Profile />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="*" element={<NotFound />} />
-        </Routes>
-        </>
+        routes()
     );
 }
