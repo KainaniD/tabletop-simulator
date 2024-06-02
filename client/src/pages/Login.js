af777c4a89857f491154739fa528ca0717048222
KainaniD <kainanime2@gmail.com>
2024-06-02 07:08:23 -0700

    refactored login and register page styles

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -22,36 +22,38 @@ export function Login() {
 
     return (
         <div className="flex justify-center pt-20">
-            <div className="w-1/4 items-center">
+            <div className="flex flex-col w-1/4">
                 <h1 className="text-center"> Login </h1>
-                <form onSubmit={handleSubmit} className="items-center">
-                    <div className="flex space-x-2">
-                    <label htmlFor="username" className="input-label">Username</label>
-                    <input
-                        type="text"
-                        placeholder="Enter Username"
-                        autoComplete="off"
-                        name="username"
-                        className="input-box"
-                        onChange={(e) => setUsername(e.target.value)}
-                    />
-                    </div>
-                    <div className="flex space-x-2">
-                    <label htmlFor="password" className="input-label">Password</label>
-                    <input
-                        type="password"
-                        placeholder="Enter Password"
-                        name="password"
-                        className="input-box"
-                        onChange={(e) => setPassword(e.target.value)}
-                    />
+                <form onSubmit={handleSubmit} className="flex items-center gap-5">
+                    <div>
+                        <div className="flex space-x-2">
+                            <label htmlFor="username" className="input-label">Username</label>
+                            <input
+                                type="text"
+                                placeholder="Enter Username"
+                                autoComplete="off"
+                                name="username"
+                                className="input-box"
+                                onChange={(e) => setUsername(e.target.value)}
+                            />
+                        </div>
+                        <div className="flex space-x-2">
+                            <label htmlFor="password" className="input-label">Password</label>
+                            <input
+                                type="password"
+                                placeholder="Enter Password"
+                                name="password"
+                                className="input-box"
+                                onChange={(e) => setPassword(e.target.value)}
+                            />
+                        </div>
                     </div>
                     <button type="submit" className="py-5 px-10 rounded-lg bg-blue-400 motion-safe:hover:bg-blue-500">
                         Login
                     </button>
                 </form>
             </div>
-            <div className="flex flex-col w-1/4 justify-center items-center">
+            <div className="flex flex-col w-1/4 items-center">
                 <p className="mb-5">Don't have an account?</p>
                 <Link to="/Register" className="py-5 px-10 my-1 rounded-lg bg-blue-400 motion-safe:hover:bg-blue-500">Sign-up</Link>
             </div>
