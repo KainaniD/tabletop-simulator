4793fd6f50702d89e60433582f4e8eba894625fe
Keelan Hu <keelanhu01@g.ucla.edu>
2024-06-03 21:39:03 -0700

    Fixed bug with dragging into playerhand

--- client/src/game/gameobjects/card.js
+++ client/src/game/gameobjects/card.js
@@ -52,18 +52,15 @@ class Card extends Phaser.GameObjects.Image {
                     this.playerHand=target;
                 }
                 target.handRender()
-
-                
-
             }
         )
 
-        this.on('dragleave',
-            function(pointer, target) {
-                target.removeCard(this);
-                this.playerHand=null;
-
-                
+        this.on('dragstart',
+            function(pointer, dragX, dragY) {
+                if(pointer.duration < 150 && this.playerHand ){
+                    this.playerHand.removeCard(this)
+                    this.playerHand=null;
+                }
             }
         
         )
