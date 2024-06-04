73ca88ab497c94bf43948ff84a4783cad300c6af
KainaniD <kainanime2@gmail.com>
2024-06-03 18:40:31 -0700

    added secure cookies

--- server/index.js
+++ server/index.js
@@ -38,7 +38,7 @@ app.use(session({
     resave: false,
     saveUninitialized: true,
     cookie: {
-        secure: false,
+        secure: true,
         sameSite: 'lax',
         maxAge: 1000 * 60 * 60 * 24
     }
