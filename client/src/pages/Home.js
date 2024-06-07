43b86c70a72467d69e34ae276af7d4fe36eaeea9
Patrick Zhou <patrickxc.zhou@gmail.com>
2024-06-06 22:06:34 -0700

    changed the homepage so theres content now

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -5,6 +5,13 @@ import hotdude from '../assets/PATRICKHOT.jpg'
 import siddmouth from '../assets/SIDDBIG.jpg'
 import keelan from '../assets/KEELANSELFIE.png'
 import game from '../assets/finishgame.png'
+import navbar from '../assets/navbar.png'
+import gameroom from '../assets/gamerooms.png'
+import room from '../assets/room.png'
+import pfp from '../assets/pfpimage.png'
+import friendreqs from '../assets/friendReqs.png'
+import friendaccept from '../assets/friendSend.png'
+import solitaire from '../assets/solitaire.jpg'
 
 export const Home = () => {
     return (
@@ -18,6 +25,43 @@ export const Home = () => {
                     </div>
                     <div className='px-5 pt-4'>
                         <div className='h-100 rounded-lg border-4 overflow-y-auto p-2'>
+                            <div className='font-bold text-xl'>
+                                Aim Trainer (2 OR MORE Players)
+                            </div>
+                            <p>
+                                Have one player drag a playing card around the screen and have the other players try to right click on it to flip it over!
+                            </p>
+                            <div className='font-bold text-xl'>
+                                GTA6 (1 OR MORE Players)
+                            </div>
+                            <p>
+                                Great if you have a nice imagination and are schizophrenic!
+                            </p>
+                            <p>
+                                Pretend each card is one of the characters and drag them around like NPC's!
+                            </p>
+                            <div className='font-bold text-xl'>
+                                Clash Royale (2 Players)
+                            </div>
+                            <p>
+                                Pretend each one is two lanes and label each one of the cards as real Clash Royale characters!
+                            </p>
+                            <p>
+                                Helps if you make the noises of each character to make it more realistic and keep track of damage in the chat!
+                            </p>
+                            <div className='font-bold text-xl'>
+                                Solitaire (1 Player)
+                            </div>
+                            <p>
+                                Great for playing with yourself when you're lonely
+                            </p>
+                            <p>
+                                1. rag cards into 7 ascending columns and keep the rest as the draw deck
+                            </p>
+                            <p>
+                                2. Just like drag cards or smth so they're ascending in order and alternating
+                            </p>
+                            <img src={solitaire} className='px-5' />
                         </div>
                     </div>
                 </div>
@@ -58,11 +102,65 @@ export const Home = () => {
                 <div className="flex-cols justify-center w-1/3 bg-gray-100 rounded-lg py-5">
                     <div className='flex justify-center'>
                         <div className="text-center w-1/2 h-1/8 *:text-center border border-xl border-blue-500 bg-blue-400 rounded-lg py-2 px-2 text-2xl">
-                            Controls:
+                            Game Information:
                         </div>
                     </div>
                     <div className='px-5 pt-4'>
                         <div className='h-100 rounded-lg border-4 overflow-y-auto p-2'>
+                            <div className='text-xl font-bold'>
+                                Rooms
+                            </div>
+                            <div className='indent-8'>
+                                <p>
+                                    - Click on Rooms in the Navigation bar to see a list of all current Rooms
+                                </p>
+                                <img src={navbar} className='px-5 mb-3' />
+                                <p>
+                                    - Click on Create New Room or search for the room you want to join!
+                                </p>
+                                <img src={gameroom} className='px-5 mb-3' />
+                                <p>
+                                    - Once joined, players are loaded counterclockwise from bottom left
+                                </p>
+                                <p>
+                                    - Interact in the chat! It'll display a message when new player joins
+                                </p>
+                                <p className='pb-3'>
+                                    - Rectangular player-hands will appear as players join the room
+                                </p>
+                                <img src={room} className='px-5 mb-3' />
+                            </div>
+                            <div className='text-xl font-bold'>
+                                Controls
+                            </div>
+                            <div className='indent-8'>
+                                <p>
+                                    We went with a more lax design for enforcing rules since it allows for more kinds of games! Hold each other accountable through the in-game chat or just scream at each other in a discord call!
+                                </p>
+                                <p>
+                                    Each person has a playerhand that is a rectangluar box in one of the corners. When a player drags a card into any player's hand it's face down. Right-click a card in your playerhand to flip it over and it won't reveal for other players. If someone else clicks a card in your hand, it will flip it over for everyone though, and cards can be grabbed from other player's hands!
+                                </p>
+                                <p className='mb-3'>
+                                    Cards all come pre-shuffled and face down.
+                                </p>
+                            </div>
+                            <div className='text-xl font-bold'>
+                                Profle and Friends
+                            </div>
+                            <div className='indent-8'>
+                                <p>
+                                    - Click on the profile page to change your pfp!
+                                </p>
+                                <img src={pfp} className='px-5' />
+                                <p>
+                                    - Add your friends and they'll show up under your profile!
+                                </p>
+                                <img src={friendreqs} className='px-5' />
+                                <p>
+                                    - Accept incoming friends
+                                </p>
+                                <img src={friendaccept} className='px-5' />
+                            </div>
                         </div>
                     </div>
                 </div>
